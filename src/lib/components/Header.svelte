6f52c6e50a683497944dd211c3764ad0cbdab17b
ihaback <ihaback@fastmail.com>
2023-11-01 16:43:22 +0100

    fix logout button styling

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -21,7 +21,7 @@
 	>
 		<button
 			type="submit"
-			class="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-600 disabled:bg-blue-200"
+			class="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-slate-700 disabled:bg-slate-700"
 			value="Sign out"
 			disabled={loading}
 		>
